625c2671574c7e98a6aff9bb736e918981e14653
zgx <743267670@qq.com>
2018-11-05 20:01:48 +0800

    add laravelproject

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>404 not founded</title>
+
+        <link href="//static/admin/static/admin/lib/Hui-iconfont/1.0.8/iconfont.css" rel="stylesheet" type="text/css" /> 
+        <link href="/static/admin/static/h-ui/css/H-ui.min.css" rel="stylesheet" type="text/css" /> 
+        <link href="/static/admin/static/h-ui.admin/css/H-ui.admin.css" rel="stylesheet" type="text/css" /> 
+    </head>
+    <body>
+           <section class="container-fluid page-404 minWP text-c"> 
+               <p class="error-title"><i class="Hui-iconfont va-m" style="font-size:80px"></i> <span class="va-m"> 404</span> </p> 
+               <p class="error-description">不好意思，您访问的页面不存在~</p> 
+          </section>
+    </body>
+</html>
